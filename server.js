7e0c9ac246d3dcb84de3a370f64d0cc76107c33d
Josue1303 <80837533+Josue1303@users.noreply.github.com>
2023-10-20 01:21:34 -0600

    Get Topics

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ ConnectionString: process.env.DATABASE_URL;
 const authRoutes = require("./routes/auth");
 const formRoutes = require("./routes/form");
 const contentRoutes = require("./routes/contents");
+const topicRoutes = require("./routes/topics");
 
 // Middleware
 app.use(bodyParser.json());
@@ -22,6 +23,7 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/auth", formRoutes);
 app.use("/api/auth", contentRoutes);
+app.use("/api/auth", topicRoutes);
 
 // Test endpoint
 app.get("/test", (req, res) => {
