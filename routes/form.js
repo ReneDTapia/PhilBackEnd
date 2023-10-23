4ad04fefe22966c1af02b4743e82b39ee5975e6b
Jesús Daniel Martínez <90727612+LoGebo@users.noreply.github.com>
2023-10-23 17:35:42 -0600

    Agrego controlador para obtener todo lo respondido por un usuario en el formulario (hosted)

--- routes/form.js
+++ routes/form.js
@@ -19,4 +19,31 @@ router.get("/getForm", async (req, res) => {
   }
 }); //chocas vuelve a casa porfavor
 
+
+router.get("/getUserForm/:Users_id", async (req, res) => {
+  try {
+
+
+    const Users_id = req.params.Users_id;
+    
+    sql = `SELECT "Cuestionario"."texto", "Users_Cuestionario"."checked"
+    FROM "Users_Cuestionario"
+    INNER JOIN "Cuestionario" ON "Users_Cuestionario"."Cuestionario_id" = "Cuestionario"."id"
+    WHERE "Users_Cuestionario"."Users_id" = ${Users_id};
+    `;
+    const text = await db.query(sql, db.Sequelize.QueryTypes.SELECT);
+
+    if (text.length > 0) {
+      res.json(text);
+    } else {
+      res.status(404).json({ error: "No text was found" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}); //chocas vuelve a casa porfavor
+
+
+
+
 module.exports = router;
