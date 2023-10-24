09f9b5ec6345b47e7427ad5ba26a330a1006f91e
Jesús Daniel Martínez <90727612+LoGebo@users.noreply.github.com>
2023-10-23 21:57:23 -0600

    Actualizado con id pk Users_Cuestionario.

--- routes/form.js
+++ routes/form.js
@@ -26,11 +26,11 @@ router.get("/getUserForm/:Users_id", async (req, res) => {
 
     const Users_id = req.params.Users_id;
     
-    sql = `SELECT "Cuestionario"."texto", "Users_Cuestionario"."checked"
+    sql = `SELECT "Cuestionario"."texto", "Users_Cuestionario"."checked", "Users_Cuestionario"."Users_Cuestionario_id"
     FROM "Users_Cuestionario"
     INNER JOIN "Cuestionario" ON "Users_Cuestionario"."Cuestionario_id" = "Cuestionario"."id"
-    WHERE "Users_Cuestionario"."Users_id" = ${Users_id};
-    `;
+    WHERE "Users_Cuestionario"."Users_id" = ${Users_id}`;
+    
     const text = await db.query(sql, db.Sequelize.QueryTypes.SELECT);
 
     if (text.length > 0) {
