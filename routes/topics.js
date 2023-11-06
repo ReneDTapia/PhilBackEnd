feb2a4178ed2a1942313d5fc817c619d84cbc90f
Josue1303 <80837533+Josue1303@users.noreply.github.com>
2023-11-05 20:13:46 -0600

    TopicsInfoModel

--- routes/topics.js
+++ routes/topics.js
@@ -23,4 +23,23 @@ router.get("/getTopics/:id", async (req, res, id) => {
   }
 }); //chocas vuelve a casa porfavor
 
+router.get("/getInfoTopics/:userid", async (req, res, userid) => {
+  try {
+    const userid = req.params.userid;
+
+    sql = `SELECT * FROM "UserTopics"
+    WHERE "user" = ${userid}
+    ORDER BY id ASC`;
+    const text = await db.query(sql, db.Sequelize.QueryTypes.SELECT);
+
+    if (text.length > 0) {
+      res.json(text);
+    } else {
+      res.status(404).json({ error: "No text was found" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}); //chocas vuelve a casa porfavor
+
 module.exports = router;
