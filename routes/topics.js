ad9864b6bd5cf32258894d1081b4e38d55607dea
Josue1303 <80837533+Josue1303@users.noreply.github.com>
2023-11-05 22:27:29 -0600

    Nuevo Get Topic

--- routes/topics.js
+++ routes/topics.js
@@ -3,33 +3,27 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/getTopics/:id", async (req, res, id) => {
+router.get("/getTopics/:userId/:id", async (req, res) => {
   try {
     const id = req.params.id;
+    const userId = req.params.userId;
 
-    sql = `SELECT id, title, description
-    FROM "Topics"
-    WHERE content = ${id}
-    ORDER BY id ASC`;
-    const text = await db.query(sql, db.Sequelize.QueryTypes.SELECT);
-
-    if (text.length > 0) {
-      res.json(text);
-    } else {
-      res.status(404).json({ error: "No text was found" });
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-}); //chocas vuelve a casa porfavor
-
-router.get("/getInfoTopics/:userid", async (req, res, userid) => {
-  try {
-    const userid = req.params.userid;
-
-    sql = `SELECT * FROM "UserTopics"
-    WHERE "user" = ${userid}
-    ORDER BY id ASC`;
+    sql = `SELECT 
+    "UserTopics"."id" AS user_topic_id,
+    "UserTopics"."done" AS done,
+    "UserTopics"."user" AS "user",
+    "Topics"."id" AS topic,
+    "Topics"."title" AS title,
+    "Topics"."description" AS description,
+    "Topics"."content" AS "content"
+    FROM (
+      SELECT *
+      FROM "UserTopics"
+      WHERE "UserTopics"."user" = ${userId}
+    ) AS "UserTopics"
+    RIGHT JOIN "Topics" ON "UserTopics"."topic" = "Topics"."id"
+    WHERE "Topics"."content" = ${id}
+    ORDER BY "topic" ASC`;
     const text = await db.query(sql, db.Sequelize.QueryTypes.SELECT);
 
     if (text.length > 0) {
