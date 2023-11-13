be4ca4524ba38dcf9a4d7fdb28fbd7adccc53251
Jesús Daniel Martínez <90727612+LoGebo@users.noreply.github.com>
2023-11-12 19:51:48 -0600

    actualizacion mensajes

--- routes/message.js
+++ routes/message.js
@@ -30,9 +30,10 @@ router.get("/getUserConversations/:userId", async (req, res) => {
 
       const sql = `
           SELECT c."conversationId", c.name, MAX(m."sendAt") as "lastMessageAt"
-          FROM "Message" m
-          JOIN "Conversation" c ON m."conversationId" = c."conversationId"
-          WHERE m."user" = ${userId} AND m."conversationId" IS NOT NULL
+          FROM "Users_Conversation" uc
+          JOIN "Conversation" c ON uc."Conversation_conversationId" = c."conversationId"
+          LEFT JOIN "Message" m ON c."conversationId" = m."conversationId"
+          WHERE uc."Users_id" = ${userId}
           GROUP BY c."conversationId", c.name
           ORDER BY "lastMessageAt" DESC`;
 
@@ -47,6 +48,7 @@ router.get("/getUserConversations/:userId", async (req, res) => {
       res.status(500).json({ error: err.message });
   }
 });
+
 
 
 router.post("/addMessage", async (req, res) => {
@@ -81,35 +83,44 @@ router.post("/addMessage", async (req, res) => {
   }
 });
 
-//crear una nueva conversation
+//crear una nueva conversation                        
 
 router.post("/addConversation", async (req, res) => {
   try {
-      const { name } = req.body;
+      const { name, userId } = req.body;
 
-      if (!name) {
-          return res.status(400).json({ error: "Please provide the conversation name" });
+      if (!name || !userId) {
+          return res.status(400).json({ error: "Please provide both the conversation name and the user ID" });
       }
 
       const escapedName = db.sequelize.escape(name);
 
-      const sql = `
+      // Insertar nueva conversación
+      let sql = `
           INSERT INTO "Conversation" ("name")
           VALUES (${escapedName})
           RETURNING "conversationId";  
       `;
+      const conversationResult = await db.query(sql, db.Sequelize.QueryTypes.INSERT);
 
-      const result = await db.query(sql, db.Sequelize.QueryTypes.INSERT);
+      const conversationId = conversationResult[0][0].conversationId;
 
-      res.status(201).json({ conversationId: result[0].conversationId });
+      // Asociar la conversación con el usuario
+      sql = `
+          INSERT INTO "Users_Conversation" ("Users_id", "Conversation_conversationId")
+          VALUES (${userId}, ${conversationId});
+      `;
+      await db.query(sql, db.Sequelize.QueryTypes.INSERT);
+
+      res.status(201).json({ conversationId : conversationId });
 
   } catch (err) {
       res.status(500).json({ error: err.message });
   }
 });
 
-//delete
 
+//delete
 router.delete("/deleteConversation/:conversationId", async (req, res) => {
   try {
       const conversationId = req.params.conversationId;
