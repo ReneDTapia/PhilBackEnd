d0d9cc59ad78c2c68627cc08db8965ac5dc1785f
Josue1303 <80837533+Josue1303@users.noreply.github.com>
2023-11-12 19:27:49 -0600

    getUserResult

--- routes/topics.js
+++ routes/topics.js
@@ -36,6 +36,26 @@ router.get("/getTopics/:userId/:id", async (req, res) => {
   }
 }); //chocas vuelve a casa porfavor
 
+router.get("/getUserResult/:userId/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const userId = req.params.userId;
+
+    sql = `SELECT COUNT(*) AS UserResult
+    FROM "UserTopics"
+    WHERE "user" = ${userId} AND "topic" = ${id}`;
+    const text = await db.query(sql, db.Sequelize.QueryTypes.SELECT);
+
+    if (text.length > 0) {
+      res.json(text);
+    } else {
+      res.status(404).json({ error: "No text was found" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}); //chocas vuelve a casa porfavor
+
 router.post("/CheckTopic", async (req, res) => {
   try {
     const { user, topic, done } = req.body;
