8b102cce32a18325118de832c0b32e593a75cdfc
Josue1303 <80837533+Josue1303@users.noreply.github.com>
2023-11-18 13:47:03 -0600

    Sections Crud

--- routes/sections.js
+++ routes/sections.js
@@ -23,5 +23,68 @@ router.get("/getSections/:id", async (req, res, id) => {
   }
 }); //chocas vuelve a casa porfavor
 
+router.post("/postSection", async (req, res) => {
+  try {
+    const { text, video, image, topic } = req.body;
+
+    const escapedText = db.sequelize.escape(text);
+    const escapedVideo = db.sequelize.escape(video);
+    const escapedImage = db.sequelize.escape(image);
+    const escapedTopic = db.sequelize.escape(topic);
+
+    const sql = `
+        INSERT INTO "Sections" ("text", "video", "image", "topic")
+        VALUES (${escapedText}, ${escapedVideo}, ${escapedImage}, ${escapedTopic})
+      `;
+
+    const result = await db.query(sql, db.Sequelize.QueryTypes.INSERT);
+
+    res.status(201).json({ messageId: result[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.put("/updateSection", async (req, res) => {
+  try {
+    const { id, text, video, image, topic } = req.body;
+
+    const escapedId = db.sequelize.escape(id);
+    const escapedText = db.sequelize.escape(text);
+    const escapedVideo = db.sequelize.escape(video);
+    const escapedImage = db.sequelize.escape(image);
+    const escapedTopic = db.sequelize.escape(topic);
+
+    const sql = `
+        UPDATE "Sections"
+        SET text = ${escapedText}, video = ${escapedVideo}, image = ${escapedImage}, topic = ${escapedTopic}
+        WHERE "id" = ${escapedId};
+      `;
+
+    const result = await db.query(sql, db.Sequelize.QueryTypes.UPDATE);
+
+    res.status(201).json({ messageId: result[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.delete("/deleteSection/:sectionId", async (req, res) => {
+  try {
+    const sectionId = req.params.sectionId;
+
+    // Primero, eliminar referencias en Users_Conversation
+    let sql = `DELETE FROM "Sections"
+    WHERE "id" = ${sectionId}`;
+
+    await db.query(sql, db.Sequelize.QueryTypes.DELETE);
+
+    res.status(200).json({
+      message: "Conversation and associated messages deleted successfully",
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
