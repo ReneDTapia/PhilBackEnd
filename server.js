010dcf4f1811678bce8e218c4bcab2a1cab4bf8f
Rene Dario Tapia Alcaraz <88797997+ReneDTapia@users.noreply.github.com>
2023-11-18 18:12:16 -0600

    panel de admin

--- server.js
+++ server.js
@@ -1,12 +1,61 @@
+require("dotenv").config();
 const pg = require("pg");
 const { config } = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const db = require('./models/index')
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
 //config();
 
+
+(async () => {
+  const AdminJS = (await import('adminjs')).default;
+  const AdminJSExpress = (await import('@adminjs/express')).default;
+  const AdminJSSequelize = (await import('@adminjs/sequelize')).default;
+
 const app = express();
+
+  app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  // Puedes agregar más opciones de configuración aquí si es necesario
+}));
+
+
+// Configuración de AdminJS
+AdminJS.registerAdapter(AdminJSSequelize);
+const adminJs = new AdminJS({
+  databases: [db.sequelize],
+  rootPath: '/admin',
+});
+
+// Configuración del enrutador AdminJS con autenticación básica
+const adminRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+  authenticate: async (email, password) => {
+    let user = await db.User.findOne({ where: { email: email } });
+
+    if (!user) {
+      user = await db.User.findOne({ where: { username: email } });
+    }
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (isPasswordValid) {
+        return { email: user.email, id: user.id }; // Retorna un objeto de usuario si la autenticación es exitosa
+      }
+    }
+
+    return false; // Retorna false si la autenticación falla
+  },
+  cookieName: 'adminjs', // Puedes personalizar el nombre de la cookie si lo deseas
+  cookiePassword: process.env.ADMIN_COOKIE_SECRET,
+});
+
+
+
 
 //const pool = new pg.pool({
 ConnectionString: process.env.DATABASE_URL;
@@ -22,6 +71,11 @@ const sectionRoutes = require("./routes/sections");
 const analRoutes = require("./routes/analytics");
 
 // Middleware
+
+// AdminJS
+app.use(adminJs.options.rootPath, adminRouter);
+
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
@@ -38,4 +92,13 @@ app.get("/test", (req, res) => {
   res.json({ message: "Test successful" });
 });
 
+const PORT = process.env.PORT || 5005;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
 module.exports = app; // Exporta la aplicación
+
+
+  })();
+
