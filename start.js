3e0962937fb1f67cc853ca774f0d3fbf73793e28
LuisFerP12 <102192932+LuisFerP12@users.noreply.github.com>
2023-11-20 21:47:01 -0600

    Update start.js

--- start.js
+++ start.js
@@ -1,1 +1,13 @@
 
+¿const createApp = require('./server');
+
+async function startServer() {
+  const app = await createApp();
+  const PORT = process.env.PORT || 5005;
+
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+}
+
+startServer();
