3dd3e6d135af890622ce0b7dd06206d049ca62ad
LG <92387298+LGnan@users.noreply.github.com>
2023-11-20 21:53:28 -0600

    ?

--- models/index.js
+++ models/index.js
@@ -42,12 +42,6 @@ db.query = async (sql, queryType) => {
   }
 };
 
-<<<<<<< Updated upstream
-
-
-
-db.User = require('./user')(sequelize, Sequelize.DataTypes);
-=======
 // Definir modelos aquí
 db.Conversation = require("./conversation")(sequelize, Sequelize.DataTypes);
 db.Message = require("./messageModel")(sequelize, Sequelize.DataTypes);
@@ -102,7 +96,6 @@ Object.keys(db).forEach((modelName) => {
     db[modelName].associate(db);
   }
 });
->>>>>>> Stashed changes
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
