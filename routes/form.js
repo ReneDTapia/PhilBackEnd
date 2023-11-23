b72e087298b36847bb961b8177384368ea1f415e
Jesús Daniel Martínez <90727612+LoGebo@users.noreply.github.com>
2023-11-23 16:49:12 -0600

    Get user form por que el chocas lo borro falta pasar al ORM.

--- routes/form.js
+++ routes/form.js
@@ -46,6 +46,27 @@ router.post("/postForm", async (req, res) => {
   }
 });
 
+router.get("/getUserForm/:Users_id", async (req, res) => {
+  try {
+    const Users_id = req.params.Users_id;
+
+    sql = `SELECT "Cuestionario"."texto", "Users_Cuestionario"."Percentage", "Users_Cuestionario"."Users_Cuestionario_id"
+    FROM "Users_Cuestionario"
+    INNER JOIN "Cuestionario" ON "Users_Cuestionario"."Cuestionario_id" = "Cuestionario"."id"
+    WHERE "Users_Cuestionario"."Users_id" = ${Users_id}`;
+
+    const text = await db.query(sql, db.Sequelize.QueryTypes.SELECT);
+
+    if (text.length > 0) {
+      res.json(text);
+    } else {
+      res.status(404).json({ error: "No text was found" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}); //chocas vuelve a casa porfavor
+
 router.post("/postUserForm", async (req, res) => {
   try {
     const answers = req.body;
