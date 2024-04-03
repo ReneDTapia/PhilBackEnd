148b77d4b9d40b8865756b270f4b66b1daefc403
LuisFerP12 <102192932+LuisFerP12@users.noreply.github.com>
2024-04-03 11:38:36 -0600

    cambios2

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
-const app = require("./server");
+const app = require("./server2");
 
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
