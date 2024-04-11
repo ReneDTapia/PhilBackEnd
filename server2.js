84f9544a5fb346f4d2cd0989b6c75d4282aae982
Josue1303 <80837533+Josue1303@users.noreply.github.com>
2024-04-11 12:50:10 -0600

    Solucion para luisao

--- server2.js
+++ server2.js
@@ -21,9 +21,7 @@ const formRoutes = require("./routes/form");
 const topicRoutes = require("./routes/topics");
 const messageRoutes = require("./routes/message");
 const contentRoutes = require("./routes/contents");
-const photosRoutes = require("./routes/photos");
 const sectionRoutes = require("./routes/sections");
-const analRoutes = require("./routes/analytics");
 
 // Middleware
 app.use(bodyParser.json());
@@ -33,9 +31,7 @@ app.use("/api/auth", formRoutes);
 app.use("/api/auth", topicRoutes);
 app.use("/api/auth", messageRoutes);
 app.use("/api/auth", contentRoutes);
-app.use("/api/auth", photosRoutes);
 app.use("/api/auth", sectionRoutes);
-app.use("/api/auth", analRoutes);
 
 // Test endpoint
 app.get("/test", (req, res) => {
